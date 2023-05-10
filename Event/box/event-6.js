6c6613b18e5bc6e6e8f9cd4973289982dc2d377b
zelonKim <ksz18601@gmail.com>
2023-05-10 19:08:58 +0900

    event2

---
+++ Event/box/event-6.js
@@ -0,0 +1,34 @@
+/* 
+const box = document.querySelector("#box");
+
+box.addEventListener("click", (e) => {
+  alert(`이벤트 발생 위치 : ${e.pageX}, ${e.pageY}`);
+}); 
+*/
+
+
+/* 
+const box = document.querySelector("#box");
+
+box.addEventListener("click", (e) => {
+  alert(`${e.target}`) // 이벤트가 발생한 대상을 반환함.
+} ) 
+*/
+
+/* 
+const box = document.querySelector("#box");
+
+box.addEventListener("click", (e) => {
+  alert(`${e.timeStamp}`) // 이벤트가 발생한 시간을 밀리초 단위로 반환함.
+} )
+*/
+
+/* 
+const box = document.querySelector("#box");
+
+box.addEventListener("click", (e) => {
+  alert(`${e.type}`) // 발생한 이벤트명을 반환함.
+} )
+*/
+
+
