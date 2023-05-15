4eeb7f8b89dd9441b9f5925c3dd326655e785ec2
zelonKim <ksz18601@gmail.com>
2023-05-15 20:10:12 +0900

    wrapper object

---
+++ String_and_Array/app.js
@@ -0,0 +1,24 @@
+/* 
+let str = "hello"
+
+console.log(str.__proto__) // String {'', constructor: ƒ, anchor: ƒ, at: ƒ, big: ƒ, …}
+*/
+
+/* 
+function counting(str, ch) {
+    let count = 0;
+
+    for (let i=0; i<str.length; i++) {
+        if(str[i] === ch)
+            count += 1;
+    }
+    return count
+}
+
+const string = prompt("문자열을 입력하세요");
+const letter = prompt("어떤 문자를 체크하시겠습니까?");
+
+const result = counting(string, letter);
+document.write(`${string}에는 ${letter}가 ${result}개 있습니다.`) 
+*/
+
